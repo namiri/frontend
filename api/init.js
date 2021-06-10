2f78ce4cd4153b00afb262e84d391b7e8fc24ccb
amiri <n.amiry70@gmail.com>
2021-06-10 22:27:17 +0430

    git commit -m "feat login"

--- api/init.js
+++ api/init.js
@@ -3,5 +3,5 @@ import { baseURL } from '~/config'
 import cookies from 'js-cookie'
 import { setAuthToken } from '~/utils/auth'
 axios.defaults.baseURL = baseURL
-const token = cookies.get('access_token')
+const token = cookies.get('token')
 if (token) setAuthToken(token)
